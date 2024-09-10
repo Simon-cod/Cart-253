2c6a11788dcab865e8d06347da35450022e19b5d
Simon-cod <sduchaine2004@gmail.com>
2024-09-10 16:43:16 -0400

    Drew a beutiful purple record
    Developed a different program

--- Topics/2nd week/drawing-practice/js/script.js
+++ Topics/2nd week/drawing-practice/js/script.js
@@ -1,24 +1,48 @@
 /**
- * Title of Project
- * Author Name
+ * Drawing practice
+ * Simon Duchaine Morneau
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Draws a purpple record on a gray background
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a Canvas
 */
 function setup() {
-
+//a nice square canvas
+createCanvas(640, 640);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a purple record
 */
 function draw() {
+//a grey background
+background(180);
 
+//we push first to start the sequence
+push();
+//a purple circle with black outline in the middle of the canvas
+fill('purple');
+stroke(0);
+ellipse(320, 320, 480);
+//we pull at the end to reset the settings to default
+pop();
+
+// The label on the record
+push();
+fill(255, 255, 255);
+noStroke();
+ellipse(320, 320, 140, 140);
+pop();
+
+// The hole in the record
+push();
+fill(180);
+stroke(50, 50, 50);
+ellipse(320, 320, 20, 20);
+pop();
 }
