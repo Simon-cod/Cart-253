94480baef6a11d1c63c6c865e5deb276527e1323
Simon-cod <sduchaine2004@gmail.com>
2024-09-10 17:21:33 -0400

    initial commit

---
+++ Topics/2nd week/Challenges/js/script.js
@@ -0,0 +1,25 @@
+/**
+ * Challenges
+ * Simon Duchaine Morneau
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ *Drawing a canvas
+*/
+function setup() {
+drawCanvas (640, 640);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+drawBackground (100, 0, 200)
+}
