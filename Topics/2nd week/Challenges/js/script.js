b2618f33905002e1a5878bfec08b766627a93644
Simon-cod <sduchaine2004@gmail.com>
2024-09-10 17:49:00 -0400

    red and green flag

--- Topics/2nd week/Challenges/js/script.js
+++ Topics/2nd week/Challenges/js/script.js
@@ -12,7 +12,7 @@
  *Drawing a canvas
 */
 function setup() {
-drawCanvas (640, 640);
+createCanvas (400, 400);
 }
 
 
@@ -20,6 +20,27 @@ drawCanvas (640, 640);
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+//sets a light blue/purple background
+background (100, 150, 255);
 
-drawBackground (100, 0, 200)
+
+//creates a white rectangle
+push ();
+noStroke ();
+rect(35, 80, 330, 240);
+pop ();
+
+//creates the green part of the flag
+push ();
+fill(0, 150, 0)
+noStroke ();
+rect(35, 80, 110, 240);
+pop ();
+
+//creates the red part of the flag
+push ();
+fill(220, 0, 0);
+noStroke ();
+rect(255, 80, 110, 240);
+pop ();
 }
