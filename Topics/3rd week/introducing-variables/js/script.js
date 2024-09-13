dbbe1d5c6d0b0bd85aa42f6bcc5638acbeea9e8c
Simon-cod <sduchaine2004@gmail.com>
2024-09-13 14:15:44 -0400

    exprimented with variables
    created a changing sized and colored circle dependant on the center of the canvas rather than the origin (by making a substraction of half of the width and height) (I'm so smart lol)

--- Topics/3rd week/introducing-variables/js/script.js
+++ Topics/3rd week/introducing-variables/js/script.js
@@ -12,7 +12,8 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+// Create the canvas
+createCanvas(1920, 1000);
 }
 
 
@@ -20,5 +21,42 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0);
+    
+    // Draw a circle in the centre of the canvas
+    push();
+    noStroke();
+    fill(255, 255, 0);
+    // If we set the ellipse's (x,y) coordinates to
+    // *half* the width and *half* the height, it will
+    // always end up in the centre of our canvas.
+    // That's math, baby! / means division
+    ellipse(width/2, height/2, 100, 100);
+    pop();
 
+     // Draw a smaller circle
+     push();
+     noStroke();
+     fill(255, 150, 0);
+     // We use the variable names mouseX and mouseY instead
+     // of numbers for the x and y coordinates of our circle
+     // JavaScript will *use the values inside them* (the numbers)
+     // to send as the x and y arguments of ellipse()
+     // And that will mean the ellipse will be drawn with its (x, y)
+     // position set to the current mouse (x, y) position.
+     ellipse(width/2, height/2, 50, 100);
+     pop();
+     
+     // Draw a circle at the mouse coordinates
+     push();
+     noStroke();
+     fill(mouseX, mouseY, mouseX);
+     // We use the variable names mouseX and mouseY instead
+     // of numbers for the x and y coordinates of our circle
+     // JavaScript will *use the values inside them* (the numbers)
+     // to send as the x and y arguments of ellipse()
+     // And that will mean the ellipse will be drawn with its (x, y)
+     // position set to the current mouse (x, y) position.
+     ellipse(width/2, height/2, mouseX-width/2, mouseX-width/2);
+     pop();
 }
