b88fb29f38014991406d6e277dcab6f0e9e445d8
Simon-cod <sduchaine2004@gmail.com>
2024-09-13 19:34:19 -0400

    copy pasted mr furious code.
    Let the challenge begin!

--- Topics/3rd week/challenge week 3/js/script.js
+++ Topics/3rd week/challenge week 3/js/script.js
@@ -1,23 +1,44 @@
+
 /**
- * Challenge week 3
- * Simon Duchaine Morneau
- * 
- * Debugging challenge
+ * Mr. Furious
+ * Pippin Barr
+ *
+ * A guy who becomes visibly furious!
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+// Our friend Mr. Furious
+let mrFurious = {
+  // Position and size
+  x: 200,
+  y: 200,
+  size: 100,
+  // Colour
+  fill: {
+    r: 255,
+    g: 225,
+    b: 225
+  }
+};
 
+/**
+ * Create the canvas
+ */
+function setup() {
+  createCanvas(400, 400);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draw (and update) Mr. Furious
+ */
 function draw() {
-
+  background(160, 180, 200);
+  
+  // Draw Mr. Furious as a coloured circle
+  push();
+  noStroke();
+  fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+  ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  pop();
 }
