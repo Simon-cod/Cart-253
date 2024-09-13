7c1ce690e042339dd0b1958e2ee276387dcfead4
Simon-cod <sduchaine2004@gmail.com>
2024-09-13 19:32:14 -0400

    initial commit debugging challenge week 3

---
+++ Topics/3rd week/challenge week 3/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Challenge week 3
+ * Simon Duchaine Morneau
+ * 
+ * Debugging challenge
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
