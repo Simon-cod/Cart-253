cbfa01066e6b6ce88ce5618f100070d8f47d7142
Simon-cod <sduchaine2004@gmail.com>
2024-10-01 15:57:49 -0400

    Finished my art jam
    yessir!

--- Topics/3rd week/introducing-variables/js/script.js
+++ Topics/3rd week/introducing-variables/js/script.js
@@ -1,24 +1,41 @@
 /**
- * Introducing variables
+ * Color variation
  * Simon Duchaine Morneau
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * This program represents a circle changing colors to find it's personality. 
+ * Some of the most know colors/emotions are easy to access (Blue/sadness and Red/Anger) 
+ * But some colors and emotions (Yellow/happiness and Teal/peace) are harder to find 
+ * until you find the right patern.
+ *
  */
 
 "use strict";
 
+let mouseXColor = undefined;
+
+let mouseYColor = undefined;
+
+let hypothenuse = undefined;
+
+let diagonalsLength = undefined;
+
+let mouseDiagonalColor = undefined;
+
+let centerCanvas = undefined;
+
+let circleSize = undefined;
 
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a 1000 x 1000 Canvas to represnts our personality chaginig circle
 */
 function setup() {
 // Create the canvas
+
 createCanvas(1000, 1000);
 
-
 }
+
 
 
 /**
@@ -31,23 +48,23 @@ function draw() {
    
 
 //Call the function to draw the background
-drawBackground()
 
-      
+rotateBackground()
+
 
 //creates a safe zone in the middle diagonal
 if (mouseX < (mouseY + 50) && mouseX > (mouseY - 50) ) {
 
     //Draw a circle with z
-CircleWithZ()
+tealCircle()
 
      }
 else if (mouseY < (1050 - mouseX) && mouseY > (950 - mouseX)){
-CircleWithYellow()
+yellowCircle()
 }
 else {
 
-CirclreWithoutZ()
+blueAndRedCircle()
 }
 
      
@@ -55,241 +72,215 @@ CirclreWithoutZ()
 /** 
  * Create a function for a circle with fluctuing green
 */
-function CircleWithZ() {
-   
-    //Change mouse X coordinate from between 0 andf the canvas's width to between 0 and 255
-    let a = map(mouseX, 0, width, 0, 255, true);
+function tealCircle() {
 
     //Change mouse Y coordinate from between 0 and the canvas's height to between 0 and 255
-    let b = map(mouseY, 0, height, 0, 255, true);
+    mouseYColor = map(mouseY, 0, height, 0, 255);
 
     //Calculate the diagonal between mouseX and mouseY
-    let c = sqrt(mouseX ** 2 + mouseY ** 2); 
+    hypothenuse = sqrt(mouseX ** 2 + mouseY ** 2); 
 
     //Find the maximum diagonal's length
-    let diagonalsLength = sqrt(width ** 2 + height ** 2);
+    diagonalsLength = sqrt(width ** 2 + height ** 2);
 
     //Change diagonal's mouse coordinate from between 0 and diagonalsLength to between 0 and 255
-    let z = map(c, 0, diagonalsLength, 0, 255, true)  
-   
+    mouseDiagonalColor = map(hypothenuse, 0, diagonalsLength, 0, 255)  
+
     //Create a variable for the center of the canvas
-    let x = mouseX-width/2;
+    centerCanvas = mouseX-width/2;
 
     //Extend x's value to 1000
-    let xx = map(x, 0, 500, 0, 1000)
+    circleSize = map(centerCanvas, 0, 500, 0, 1000);
 
-    // // Draw a circle at the mouse coordinates
+    //Constrain the circle to the canvas's size
+    circleSize = constrain(circleSize, -width, width)
+
+    //Draw a circle at the mouse coordinates
     push();
     noStroke();
     //fill (red, green, blue)
-    fill(0, z, b);
+    fill(0, mouseDiagonalColor, mouseYColor);
     //set it in the middle of the canvas
-    ellipse(width/2, height/2, xx);
+    ellipse(width/2, height/2, circleSize);
     pop();
-    console.log("a - " + a, "b - " + b, "z - " + z, "c - " + c)
+    
 }
 
 /** 
  * Create a function for a circle with no green
 */
-function CirclreWithoutZ() {
+function blueAndRedCircle() {
     
     //Change mouse X coordinate from between 0 andf the canvas's width to between 0 and 255
-let a = map(mouseX, 0, width, 0, 255, true);
+    mouseXColor = map(mouseX, 0, width, 0, 255);
 
-//Change mouse Y coordinate from between 0 and the canvas's height to between 0 and 255
-let b = map(mouseY, 0, height, 0, 255, true);
+    //Change mouse Y coordinate from between 0 and the canvas's height to between 0 and 255
+    mouseYColor = map(mouseY, 0, height, 0, 255);
 
-//Create a variable for the center of the canvas
-let x = mouseX-width/2;
+    //Create a variable for the center of the canvas
+    centerCanvas = mouseX-width/2;
 
-//Extend x's value to 1000
-let xx = map(x, 0, 500, 0, 1000)
+    //Extend x's value to 1000
+    circleSize = map(centerCanvas, 0, 500, 0, 1000);
 
-    //Draw a circle without z
+    //Constrain the circle to the canvas's size
+    circleSize = constrain(circleSize, -width, width)
+
+    //Draw a circle without green
     push();
     noStroke();
     //fill (red, green, blue)
-    fill(a, 0, b);
+    fill(mouseXColor, 0, mouseYColor);
     //set it in the middle of the canvas
-    ellipse(width/2, height/2, xx);
+    ellipse(width/2, height/2, circleSize);
     pop();
-    console.log("a - " + a, "b - " + b)
+
 }
 /** 
  * Create a function for a circle with fluctuing green
 */
-function CircleWithYellow() {
+function yellowCircle() {
    
+    push();
     // Change the value of mouseX so that it can increase value while going to the left
-    let mx = mouseX - 1000
-
-    //Change mouse X coordinate from between 0 andf the canvas's width to between 0 and 255
-    let d = map(mouseX, width, 0, 0, 255, true);
-
-    //Change mouse Y coordinate from between 0 and the canvas's height to between 0 and 255
-    let e = map(mouseY, 0, height, 0, 255, true);
+    let newMouseX = mouseX - 1000
 
     //Calculate the diagonal between mouseX and mouseY
-    let f = sqrt(mx ** 2 + mouseY ** 2); 
+    hypothenuse = sqrt(newMouseX ** 2 + mouseY ** 2); 
 
    //Find the maximum diagonal's length
-    let diagonalsLength = sqrt(width ** 2 + height ** 2);
+    diagonalsLength = sqrt(width ** 2 + height ** 2);
 
     //Change diagonal's mouse coordinate from between 0 and diagonalsLength to between 0 and 255
-    let j = map(f, 0, diagonalsLength, 0, 255, true)  
+    let mouseInvertedDiagonalColor = map(hypothenuse, 0, diagonalsLength, 0, 255)  
 
     //Create a variable for the center of the canvas
-    let x = mouseX-width/2;
+    centerCanvas = mouseX-width/2;
 
     //Extend x's value to 1000
-    let xx = map(x, 0, 500, 0, 1000)
-   
-    // // Draw a circle at the mouse coordinates
+    circleSize = map(centerCanvas, 0, 500, 0, 1000);
+
+    //Constrain the circle to the canvas's size
+    circleSize = constrain(circleSize, -width, width)
+
+    //Draw a circle at the mouse coordinates
     push();
     noStroke();
     //fill (red, green, blue)
-    fill(255, j, 0);
+    fill(255, mouseInvertedDiagonalColor, 0);
     //set it in the middle of the canvas
-    ellipse(width/2, height/2, xx);
+    ellipse(width/2, height/2, circleSize,);
     pop();
-    console.log("d - " + d, "e - " + e, "j - " + j, "f - " + f)
+    pop();
 }
 
-function drawBackground() {
+
+function rotateBackground() {
+
+    push();
     
+    angleMode(DEGREES)
+
+    let angle = map(mouseY, 0, width, 0, 360)
+
+    translate(width/2, height/2)
+    rotate(angle)
+
     
 
     // Draw a blue circle in the centre of the canvas
     push();
     noStroke();
     fill(0, 127.5, 127.5);
-    ellipse(width/2, height/2, 1000);
-    pop();
-
-    // Draw a orange ellipse at the center of the canvas 
-     push();
-     noStroke();
-     fill(255, 127.5, 0);
-     ellipse(width/2, height/2, 25, 1000);
-     pop();
-
-    // Draw a another orange ellipse at the center of the canvas 
-     push();
-     noStroke();
-     fill(255, 127.5, 0);
-     ellipse(width/2, height/2, 1000, 25);
-     pop();
-
-      // Draw a black circle around 
-    push();
-    noStroke();
-    fill(0);
-    ellipse(1000, 1000, 1000);
-    pop();
-
-    // Draw a black circle around 
-    push();
-    noStroke();
-    fill(0);
-    ellipse(0, 1000, 1000);
-    pop();
-
-    // Draw a black circle around 
-    push();
-    noStroke();
-    fill(0);
-    ellipse(1000, 0, 1000);
-    pop();
-
-    // Draw a black circle around 
-    push();
-    noStroke();
-    fill(0);
     ellipse(0, 0, 1000);
     pop();
 
+    // Draw a orange ellipse at the center of the canvas 
+    push();
+    noStroke();
+    fill(255, 127.5, 0);
+    ellipse(0, 0, 25, 1000);
+    pop();
+
+    // Draw a another orange ellipse at the center of the canvas 
+    push();
+    noStroke();
+    fill(255, 127.5, 0);
+    ellipse(0, 0, 1000, 25);
+    pop();
+
+    // Draw a black circle around 
+    push();
+    noStroke();
+    fill(0);
+    ellipse(500, 500, 1000);
+    pop();
+
+    // Draw a black circle around 
+    push();
+    noStroke();
+    fill(0);
+    ellipse(-500, 500, 1000);
+    pop();
+
+    // Draw a black circle around 
+    push();
+    noStroke();
+    fill(0);
+    ellipse(500, -500, 1000);
+    pop();
+
+    // Draw a black circle around 
+    push();
+    noStroke();
+    fill(0);
+    ellipse(-500, -500, 1000);
+    pop();
+
     
     // Draw a black circle around 
     push();
     noStroke();
     fill(0);
-    ellipse(750, 750, 500);
+    ellipse(250, 250, 500);
     pop();
-    
-     // Draw another black circle around
-     push();
-     noStroke();
-     fill(0);
-     ellipse(250, 750, 500);
-     pop();
 
-      // Draw another black circle around 
+    // Draw another black circle around
     push();
     noStroke();
     fill(0);
-    ellipse(750, 250, 500);
+    ellipse(-250, 250, 500);
     pop();
 
-     // Draw another black circle around 
-     push();
-     noStroke();
-     fill(0);
-     ellipse(250, 250, 500);
-     pop();
+    // Draw another black circle around 
+    push();
+    noStroke();
+    fill(0);
+    ellipse(250, -250, 500);
+    pop();
 
-      // Draw an orange circle in the center
-      push();
-      noStroke();
-      fill(255, 127.5, 0);
-      ellipse(width/2, height/2, 100);
-      pop();
+    // Draw another black circle around 
+    push();
+    noStroke();
+    fill(0);
+    ellipse(-250, -250, 500);
+    pop();
 
-      // Draw a blue circle in the center
-      push();
-      noStroke();
-      fill(0, 127.5, 127.5);
-      ellipse(width/2, height/2, 50);
-      pop();
-      
+    // Draw an orange circle in the center
+    push();
+    noStroke();
+    fill(255, 127.5, 0);
+    ellipse(0, 0, 100);
+    pop();
+
+    // Draw a blue circle in the center
+    push();
+    noStroke();
+    fill(0, 127.5, 127.5);
+    ellipse(0, 0, 50);
+    pop();
+
+    pop();
 }
 
-function drawBlackCircles() {
-
-    // // Draw a blue circle in the centre of the canvas
-    //  push();
-    //  noStroke();
-    //  fill(0, 127.5, 127.5);
-    //  ellipse(width/2, height/2, 1500);
-    //  pop();
-    
-     // Draw a blue circle in the centre of the canvas
-     push();
-     noStroke();
-     fill(0);
-     ellipse(width/2, 0, 500);
-     pop();
-    
-     // Draw a blue circle in the centre of the canvas
-     push();
-     noStroke();
-     fill(0);
-     ellipse(width/2, height, 500);
-     pop();
-    
-     // Draw a blue circle in the centre of the canvas
-     push();
-     noStroke();
-     fill(0);
-     ellipse(0, height/2, 500);
-     pop();
-    
-     // Draw a blue circle in the centre of the canvas
-     push();
-     noStroke();
-     fill(0);
-     ellipse(width, height/2, 500);
-     pop();
-    
-          }
     
