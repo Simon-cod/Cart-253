0fa1b75cd45cf931d681d4841fceef6cfcb228e4
Simon-cod <sduchaine2004@gmail.com>
2024-11-02 02:39:34 -0400

    Added an animation for when the tongue returns back
    It makes it look more appealing. I also added it to the spacebar function (the reset) so instead of just magically reappearing at the frog's mouth it looks like a real tongue coming back

--- Topics/Mod Jam/js/script.js
+++ Topics/Mod Jam/js/script.js
@@ -15,9 +15,6 @@
 
 "use strict";
 
-// //!Creates an array for the fly
-// let flies = [];
-
 //?Creates a variable for the tongue's direction
 let direction = "none";
 
@@ -33,13 +30,13 @@ let frog = {
     // The frog's body has a position and size
     body: {
         x: 300,
-        y: 1800,
+        y: 1600,
         size: 150
     },
     // The frog's tongue has a position, size, speed, and state
     tongue: {
         x: 300,
-        y: 1800,
+        y: 1624,
         size: 20,
         speed: 4,
         // Determines how the tongue moves each frame
@@ -53,30 +50,28 @@ let frog = {
         },
     }
 }
-
-console.log(frog.tongue.head.x)
-// ?Our initial fly
+// TODO Our initial fly
 // Has a position, size, and speed of horizontal movement
 //Creates coordinate for the inital fly that will always reset
 const initialFly = {
     // x: 0,
     x: 0,
     y: 200, // Will be random
-    size: 10,
+    size: 15,
     speed: 3
 };
 
-//creates a variable for the borders
+// TODO creates a variable for the borders
 let border = undefined
 
-//Creates an empty array for the buggyFlies
+// TODO Creates an empty array for the buggyFlies
 let buggyFlies = []
 
 /**
  * ?Creates the canvas and initializes the fly
  */
 function setup() {
-    createCanvas(600, 2000);
+    createCanvas(600, 1624);
 
     // Give the fly its first random position
     resetFly();
@@ -96,133 +91,19 @@ function draw() {
     background("#87ceeb");
     moveFly();
     drawFly();
-    // moveFrog();
     moveTongue();
     drawTongue();
     drawFrog();
     checkTongueFlyOverlap();
-    // tongueSegments()
-    // console.log(tongueHeadCoordinates.x)
     drawsNewCrazyFly()
     
 }
 
-//creates a function that draws a new Crazy Fly (Buggy Fly)
-function drawsNewCrazyFly() {
-//reatributes the buggyFlies array to another name for the loop and creates a loop the adds fly coordinates to the array and makes  / it move
-for (let crazyFly of buggyFlies) {
-    moveCrazyFlies(crazyFly);
-    newCrazyFly();
-}
-}
-
-/**
- * ?Moves the fly according to its speed
- * ?Resets the fly if it gets all the way to the right
- */
-function moveFly() {
-    // Move the fly
-    initialFly.x += initialFly.speed;
-    // Handle the fly going off the canvas
-    if (initialFly.x > width) {
-        resetFly();
-    }
-}
-
-function moveCrazyFlies(crazyFly) {
-    let border = {
-        left: 0,
-        right: width,
-        up: 0,
-        down: height
-    }
-    
-    let firstX = border.left + (crazyFly.size/2);
-    let secondX = border.right - (crazyFly.size/2);
-    let firstY = border.up + (crazyFly.size/2);
-    let secondY = border.down - (crazyFly.size/2);
-
-    let x = constrain(crazyFly.x, firstX, secondX);
-    let y = constrain(crazyFly.y, firstY, secondY);
-
-    x += random(-crazyFly.buzziness, crazyFly.buzziness);
-    y += random(-crazyFly.buzziness, crazyFly.buzziness);
-
-    //Draws the provided fly to the canvas
-    push();
-    noStroke();
-    fill(crazyFly.r, crazyFly.g, crazyFly.b);
-    ellipse(x, y, crazyFly.size);
-    pop();
-    
-}
-
-function newCrazyFly() {
-   
-    //creates random coordinates and buzziness for each buggyFly 
-    let crazyFly = {
-        x: random(0, width),
-        y: random(0, height),
-        size: random(10, 15),
-        buzziness: 500,
-        r: 0,
-        g: 0,
-        b: 255
-    }; 
-// returns the values of the coordinates we just created
-    return crazyFly;
-}
-//     }
-// }
-
-/**
- * ?Draws the fly as a black circle
- */
-function drawFly() {
-    
-    // flies.push = (
-    push();
-    noStroke();
-    fill("#000000");
-    ellipse(initialFly.x, initialFly.y, initialFly.size);
-    pop();
-    
-}
-
-/**
- * ?Resets the fly to the left with a random y
- */
-function resetFly() {
-    initialFly.x = 0;
-    initialFly.y = random(200, 300);
-}
-
-
-
-/**
- * ?Handles the tongue overlapping the fly
- */
-function checkTongueFlyOverlap() {
-    // Get distance from tongue to fly
-    const d = dist(tongueHeadCoordinates.x, tongueHeadCoordinates.y, initialFly.x, initialFly.y);
-
-    // Check if it's an overlap
-    const eaten = (d < frog.tongue.size/2 + initialFly.size/2);
-    if (eaten) {
-        //Creates a Buggy Fly
-        addsCrazyFlyToTheArray();
-        // Reset the fly
-        resetFly();
-        // Bring back the tongue
-        frog.tongue.direction= "none";
-    }
-}
-
 /**
  * ?Draws the tongue as a vector
- */
+*/
 function drawTongue() {
-//Draws the tongue vector
+    //Draws the tongue vector
     push();
      stroke(150, 0, 250);
      strokeWeight(frog.tongue.size)
@@ -238,87 +119,98 @@ function drawTongue() {
     // console.log("Head Coordinates =" + tongueHeadCoordinates, "Origins =" + changedTongueOrigin)
 }
 
+    /**
+     * ?Displays the frog (body)
+     */
+    function drawFrog() {
 
-/**
- * ?Displays the frog (body)
- */
-function drawFrog() {
+        // Draw the frog's body
+        push();
+        fill(50, 150, 250);
+        stroke(0, 0, 200);
+        ellipse(frog.body.x, frog.body.y, frog.body.size);
+        pop();
 
-    // Draw the frog's body
-    push();
-    fill(50, 150, 250);
-    stroke(0, 0, 200);
-    ellipse(frog.body.x, frog.body.y, frog.body.size);
-    pop();
+        //Draw spots
+        push();
+        fill(0, 0, 200);
+        noStroke();
+        ellipse(frog.body.x - 45, frog.body.y, 60);
+        pop();
 
-    //Draw spots
-    push();
-    fill(0, 0, 200);
-    noStroke();
-    ellipse(frog.body.x - 45, frog.body.y, 60);
-    pop();
+        push();
+        fill(0, 0, 200);
+        noStroke();
+        ellipse(frog.body.x + 30, frog.body.y - 35, 60);
+        pop();
 
-    push();
-    fill(0, 0, 200);
-    noStroke();
-    ellipse(frog.body.x + 30, frog.body.y - 35, 60);
-    pop();
-
-    push();
-    fill(0, 0, 200);
-    noStroke();
-    ellipse(frog.body.x - 20, frog.body.y - 45, 30);
-    pop();
+        push();
+        fill(0, 0, 200);
+        noStroke();
+        ellipse(frog.body.x - 20, frog.body.y - 45, 30);
+        pop();
     
-    push();
-    fill(0, 0, 200);
-    noStroke();
-    ellipse(frog.body.x + 10, frog.body.y + 20, 30);
-    pop();
+        push();
+        fill(0, 0, 200);
+        noStroke();
+        ellipse(frog.body.x + 10, frog.body.y + 20, 30);
+        pop();
 
-    push();
-    fill(0, 0, 200);
-    noStroke();
-    ellipse(frog.body.x + 60, frog.body.y + 2, 20);
-    pop();
-}
+        push();
+        fill(0, 0, 200);
+        noStroke();
+        ellipse(frog.body.x + 60, frog.body.y + 2, 20);
+        pop();
+        }
 
-/**
- * ?Moves the tongue with the keypad
- */
-function moveTongue() {
+     /**
+     * ?Moves the tongue with the keypad
+     */
+    function moveTongue() {
     
-    if (frog.tongue.stateSpeed === "fast") {
+        if (frog.tongue.stateSpeed === "fast") {
         frog.tongue.speed = 10;
-    } 
-    else {
+        } 
+        else {
         frog.tongue.speed = 4;
-    }
-
-    // If the tongue direction is none, it doesn't do anything
-    if (frog.tongue.direction === "none") {
+        }
+        // If the tongue direction is none, it doesn't do anything
+        if (frog.tongue.direction === "none") {
         // Tongue matches the frog's x
         tongueHeadCoordinates.x = frog.body.x;
-    tongueHeadCoordinates.y = (frog.body.y - frog.body.size/2);
-    }
-    else if (frog.tongue.direction === "up") {
+        tongueHeadCoordinates.y = (frog.body.y - frog.body.size/2);
+        }
+        else if (frog.tongue.direction === "up") {
 
        //removes a y value so it goes up
         tongueHeadCoordinates.y -= frog.tongue.speed;
-    }
-    else if (frog.tongue.direction === "right") {
+        }
+        else if (frog.tongue.direction === "right" && tongueHeadCoordinates.y < (frog.body.y - frog.body.size/2)) {
        
         tongueHeadCoordinates.x += frog.tongue.speed;
 
-    }
-    else if (frog.tongue.direction === "left") {
+        }
+        else if (frog.tongue.direction === "left" && tongueHeadCoordinates.y < (frog.body.y - frog.body.size/2)) {
         tongueHeadCoordinates.x -= frog.tongue.speed 
-    }
-    else if (frog.tongue.direction === "down") {
+        }
+        else if (frog.tongue.direction === "down" && tongueHeadCoordinates.y < (frog.body.y - frog.body.size/2)) {
         tongueHeadCoordinates.y += frog.tongue.speed
-    }
-
-    if (frog.tongue.state === "idle") {
+        }
+        else if (frog.tongue.direction === "goingBack" && tongueHeadCoordinates.x < (width/2) - 4) {
+                tongueHeadCoordinates.x += 10
+        } else if ( frog.tongue.direction === "goingBack" && tongueHeadCoordinates.x > (width/2) + 4) {
+            tongueHeadCoordinates.x -= 10
+        }
+        else if (frog.tongue.direction === "goingBack" && tongueHeadCoordinates.x <= (width/2) + 4 && tongueHeadCoordinates.x >= (width/2) - 4  ) {
+        
+            if (tongueHeadCoordinates.y < frog.body.y) {
+                tongueHeadCoordinates.y += 30
+            } else {
+                frog.tongue.direction = "none"
+            }
+        }
+        
+        if (frog.tongue.state === "idle") {
             // Do nothing
         }
         // The tongue bounces back if it hits the top
@@ -326,51 +218,218 @@ function moveTongue() {
             frog.tongue.state = "inbound";
         
         }
-    // If the tongue is inbound, it moves down
-    else if (frog.tongue.state === "inbound") {
+        // If the tongue is inbound, it moves down
+        else if (frog.tongue.state === "inbound") {
         frog.tongue.y += frog.tongue.speed;
         // The tongue stops if it hits the bottom
         if (frog.tongue.y >= height) {
             frog.tongue.state = "idle";
         }
+        }
     }
-}
 
-
-/**
- * ? Changes tongue state when you press on the keypad
- */
-function keyPressed() {
-    // if (frog.tongue.state === "idle") {
-    //     frog.tongue.state = "outbound";
-    if (keyCode === UP_ARROW) {
+     /**
+     * ?Changes the tongue state when you press on the keypad
+     */
+    function keyPressed() {
+        // if (frog.tongue.state === "idle") {
+        //     frog.tongue.state = "outbound";
+        if (keyCode === UP_ARROW && frog.tongue.direction !== "goingBack") {
         frog.tongue.direction = "up"; //moves 0 along x and -1 (up) along y axis
-  } else if (keyCode === DOWN_ARROW) {
-    frog.tongue.direction = "down";
-  } else if (keyCode === RIGHT_ARROW) {
-    frog.tongue.direction = "right";
-  } else if (keyCode === LEFT_ARROW) {
-    frog.tongue.direction = "left"
-  } //Resets the direction to none if spacebar is pressed
-  else if (keyCode === 32) {
-    frog.tongue.direction = "none"
-  } //Changes the tongue speed to fast when the shift key is pressed
-  else if (keyCode === 16) {
-    frog.tongue.stateSpeed = "fast"
-  } 
+        } else if (keyCode === DOWN_ARROW && frog.tongue.direction !== "goingBack") {
+        frog.tongue.direction = "down";
+        } else if (keyCode === RIGHT_ARROW && frog.tongue.direction !== "goingBack") {
+        frog.tongue.direction = "right";
+        } else if (keyCode === LEFT_ARROW && frog.tongue.direction !== "goingBack") {
+        frog.tongue.direction = "left"
+        } //Resets the direction to none if spacebar is pressed
+        else if (keyCode === 32) {
+        frog.tongue.direction = "goingBack"
+        } //Changes the tongue speed to fast when the shift key is pressed
+        else if (keyCode === 16 && frog.tongue.direction !== "goingBack") {
+        frog.tongue.stateSpeed = "fast"
+         } 
+    }
+
+    /**
+     * ? Removes the speed effect when you release the shift key
+     */
+    function keyReleased() {
+        if (keyCode === 16) {
+        frog.tongue.stateSpeed = "normal"
+        }
 }
+
 
 /**
- * ? Removes the speed effect when you release the shift key
+ * TODO creates a function that draws a new Crazy Fly (Buggy Fly)
  */
-function keyReleased() {
-    if (keyCode === 16) {
-    frog.tongue.stateSpeed = "normal"
+function drawsNewCrazyFly() {
+    //reatributes the buggyFlies array to another name for the loop and creates a loop the adds fly coordinates to the array and makes  / it move
+    for (let crazyFly of buggyFlies) {
+        moveCrazyFlies(crazyFly);
+        newCrazyFly();
     }
-}
+    }
+    
+    /**
+     * TODO Moves the fly according to its speed
+     * TODO Resets the fly if it gets all the way to the right
+     */
+    function moveFly() {
+        // Move the fly
+        initialFly.x += initialFly.speed;
+        // Handle the fly going off the canvas
+        if (initialFly.x > width) {
+            resetFly();
+        }
+    }
+    
+    /**
+     * TODO Makes the buggy Fly move in a crazy way
+     */
+    function moveCrazyFlies(crazyFly) {
+        let border = {
+            left: 0,
+            right: width,
+            up: 0,
+            down: height
+        }
+        
+        let firstX = border.left + (crazyFly.size/2);
+        let secondX = border.right - (crazyFly.size/2);
+        let firstY = border.up + (crazyFly.size/2);
+        let secondY = border.down - (crazyFly.size/2);
+    
+        let x = constrain(crazyFly.x, firstX, secondX);
+        let y = constrain(crazyFly.y, firstY, secondY);
+    
+        x += random(-crazyFly.buzziness, crazyFly.buzziness);
+        y += random(-crazyFly.buzziness, crazyFly.buzziness);
+    
+        //Draws the provided fly to the canvas
+        push();
+        noStroke();
+        fill(crazyFly.r, crazyFly.g, crazyFly.b);
+        ellipse(x, y, crazyFly.size);
+        pop();
+        
+    }
+    
+    /**
+     * TODO Creates the coordinate for a new buggy Fly
+     */
+    function newCrazyFly() {
+       
+        //creates random coordinates and buzziness for each buggyFly 
+        let crazyFly = {
+            x: random(0, width),
+            y: random(0, height),
+            size: random(15, 20),
+            buzziness: 500,
+            r: 0,
+            g: 0,
+            b: 255
+        }; 
+    // returns the values of the coordinates we just created
+        return crazyFly;
+    }
+    
+    /**
+     * TODO Draws the initial fly as a black circle
+     */
+    function drawFly() {
+        
+        // flies.push = (
+        push();
+        noStroke();
+        fill("#000000");
+        ellipse(initialFly.x, initialFly.y, initialFly.size);
+        pop();
+        
+    }
+    
+    /**
+     * TODO Resets the initial fly to the left with a random y
+     */
+    function resetFly() {
+        initialFly.x = 0;
+        initialFly.y = random(200, 300);
+    }
+    
+    
+    /**
+     * TODO Handles the tongue overlapping the fly and creates a new buggy fly everytume it overlaps, as well as resetting the initial fly location
+     */
+    function checkTongueFlyOverlap() {
+        // Get distance from tongue to fly
+        const d = dist(tongueHeadCoordinates.x, tongueHeadCoordinates.y, initialFly.x, initialFly.y);
+    
+        // Check if it's an overlap
+        const eaten = (d < frog.tongue.size/2 + initialFly.size/2);
+        if (eaten) {
+            //Creates a Buggy Fly
+            addsCrazyFlyToTheArray();
+            // Reset the fly
+            resetFly();
+            
+            returnsTongue();
+        }
+    }
 
-function addsCrazyFlyToTheArray() {
+    function returnsTongue() {
+        
+        frog.tongue.direction = "goingBack";
+
+       
+
+        // if (tongueHeadCoordinates.x < (width/2)) {
+        //     // Bring back the tongue
+        //     for (let i = tongueHeadCoordinates.x; i < 300; i += 1) {
+        //     tongueHeadCoordinates.x += 1
+
+        //     //Draws the tongue vector
+        //     push();
+        //     stroke(150, 0, 250);
+        //     strokeWeight(frog.tongue.size)
+        //     point(tongueHeadCoordinates)
+        //     pop();
+
+        //     // Draw the body of the tongue (that follows the tongue's)
+        //      push();
+        //     stroke(150, 50, 250);
+        //     strokeWeight(frog.tongue.size);
+        //     line(tongueHeadCoordinates.x, tongueHeadCoordinates.y, changedTongueOrigin.x, changedTongueOrigin.y);
+        //     pop();
+            
+        //     }
+        // } else if (tongueHeadCoordinates.x > (width/2)) {
+        //     for (let i = tongueHeadCoordinates.x; i > 300; i -= 1) {
+        //     tongueHeadCoordinates.x -= 1
+
+        //     //Draws the tongue vector
+        //     push();
+        //     stroke(150, 0, 250);
+        //     strokeWeight(frog.tongue.size)
+        //     point(tongueHeadCoordinates)
+        //     pop();
+
+        //     // Draw the body of the tongue (that follows the tongue's)
+        //      push();
+        //     stroke(150, 50, 250);
+        //     strokeWeight(frog.tongue.size);
+        //     line(tongueHeadCoordinates.x, tongueHeadCoordinates.y, changedTongueOrigin.x, changedTongueOrigin.y);
+        //     pop();
+            
+        //     }
+        // }
+        // frog.tongue.direction = "goingBack";
+    }
+    /** 
+     * TODO Adds a new buggy Fly coordinates to the biggyFlies Array
+     */
+    function addsCrazyFlyToTheArray() {
     let randomFly = newCrazyFly();
     buggyFlies.push(randomFly)
-    }
+}
 
