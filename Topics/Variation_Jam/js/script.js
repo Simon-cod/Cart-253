ec33a0195a23c3b46be543d8c4386598e2832089
Simon-cod <sduchaine2004@gmail.com>
2024-11-08 23:35:09 -0500

    Initial Commit

---
+++ Topics/Variation_Jam/js/script.js
@@ -0,0 +1,26 @@
+/**
+ * Cowboy cube
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+    createCanvas(1000, 1000)
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
